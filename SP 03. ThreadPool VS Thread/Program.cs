39c44f572dcd95fca85ba9269eee2e7d31fac5ce
zamanof <zamanov@itstep.edu.az>
2025-09-11 09:18:54 +0400

    thread pool

---
+++ SP 03. ThreadPool VS Thread/Program.cs
@@ -0,0 +1,71 @@
+﻿// ThreadPool VS Thread
+
+
+using System.Diagnostics;
+
+int operationCount = 1000;
+var watch = new Stopwatch();
+//watch.Start();
+//usethread(operationcount);
+
+watch.Stop();
+Console.WriteLine($"{watch.ElapsedMilliseconds} milliseconds");
+
+watch.Reset();
+
+watch.Start();
+//UseThreadPool(operationCount);
+
+watch.Stop();
+Console.WriteLine($"{watch.ElapsedMilliseconds} milliseconds");
+
+void UseThread(int operationCount)
+{
+    List<int> threadsIds = new List<int>();
+    using (var count = new CountdownEvent(operationCount))
+    {
+        Console.WriteLine("Threads...");
+        for (int i = 0; i < operationCount; i++)
+        {
+            var thread = new Thread(() =>
+            {
+                Thread.Sleep(1);
+                //Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId}");
+                if (!threadsIds.Contains(Thread.CurrentThread.ManagedThreadId))
+                {
+                    threadsIds.Add(Thread.CurrentThread.ManagedThreadId);
+                }
+                count.Signal();
+            });
+            thread.Start();
+        }
+        count.Wait();
+    }
+    Console.WriteLine($"Thread Used {threadsIds.Count} threads");
+
+}
+
+void UseThreadPool(int operationCount)
+{
+    List<int> threadsIds = new List<int>();
+    using (var count = new CountdownEvent(operationCount))
+    {
+        Console.WriteLine("ThreadPool...");
+        for (int i = 0; i < operationCount; i++)
+        {
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                Thread.Sleep(1);
+                //Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId}");
+                if (!threadsIds.Contains(Thread.CurrentThread.ManagedThreadId))
+                {
+                    threadsIds.Add(Thread.CurrentThread.ManagedThreadId);
+                }
+                count.Signal();
+            });
+        }
+            count.Wait();
+    }
+        Console.WriteLine($"ThreadPool Used {threadsIds.Count} threads");
+
+}
---
+++ SP 03. ThreadPool/Program.cs
@@ -0,0 +1,54 @@
+﻿// ThreadPool
+
+/*
+CLR Thread Pool thread-lərini istifadə edir:
+
+1. class ThreadPool
+2. WinForm Timer
+3. Asinxron metodlar (.BeginInvoke(), .EndInvoke()) - köhnəlmiş
+4. TPL - Task Parallel Library
+...
+
+*/
+
+
+
+//ThreadPool.GetAvailableThreads(out int workerCount, out int complCount);
+
+//Console.WriteLine(workerCount);
+//Console.WriteLine(complCount);
+
+
+Console.WriteLine("Main method start...");
+
+ThreadPool.QueueUserWorkItem(SomeOperations!, "Salam");
+ThreadPool.QueueUserWorkItem(o => 
+{ 
+    OtherOperations(); 
+});
+
+Console.WriteLine($"Main method ThreadId {Thread.CurrentThread.ManagedThreadId}");
+Console.WriteLine($"Main method IsBackground: {Thread.CurrentThread.IsBackground}");
+Console.WriteLine($"Main method IsThreadPool: {Thread.CurrentThread.IsThreadPoolThread}");
+
+Console.WriteLine("Main method end...");
+
+
+void SomeOperations(object state)
+{
+    Console.WriteLine("\tSomeOperations method start...");
+    Console.WriteLine($"\tSomeOperations method state value: {state}");
+    Console.WriteLine($"\tSomeOperations method ThreadId {Thread.CurrentThread.ManagedThreadId}");
+    Console.WriteLine($"\tSomeOperations method IsBackground: {Thread.CurrentThread.IsBackground}");
+    Console.WriteLine($"\tSomeOperations method IsThreadPool: {Thread.CurrentThread.IsThreadPoolThread}");
+    Console.WriteLine("\tSomeOperations method end...");
+}
+
+void OtherOperations()
+{
+    Console.WriteLine("\t\tOtherOperations method start...");
+    Console.WriteLine($"\t\tOtherOperations method ThreadId {Thread.CurrentThread.ManagedThreadId}");
+    Console.WriteLine($"\t\tOtherOperations method IsBackground: {Thread.CurrentThread.IsBackground}");
+    Console.WriteLine($"\t\tOtherOperations method IsThreadPool: {Thread.CurrentThread.IsThreadPoolThread}");
+    Console.WriteLine(" \t\tOtherOperations method end...");
+}
